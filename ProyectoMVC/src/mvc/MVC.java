e3d0bee86c9192c90a0201c8619ec35a0ec1c450
martorarm <martorarm@PRN5-10369378>
2025-12-17 19:34:47 +0100

    Proyecto MVC - COMMIT 2 (FINAL DEL PDF)

--- ProyectoMVC/src/mvc/MVC.java
+++ ProyectoMVC/src/mvc/MVC.java
@@ -4,10 +4,23 @@
  */
 package mvc;
 
+import controlador.Controlador;
+import modelo.Modelo;
+import vista.Vista;
+
 /**
  *
  * @author martorarm
  */
 public class MVC {
-    
+    public static void main(String[] args) {
+        Modelo mod = new Modelo();
+        Vista view = new Vista();
+        
+        Controlador ctrl = new Controlador(view,mod);
+        
+        ctrl.iniciar();
+        
+        view.setVisible(true);
+    }
 }
